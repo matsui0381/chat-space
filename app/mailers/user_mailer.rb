b8f046d6ffbf985614d0a9c3554449c01b72557e
sakaue-shintaro <58502914+sakaue-shintaro@users.noreply.github.com>
2021-07-20 19:10:59 +0900

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
     def welcome_email
-      @user = params[:user]
-      mail(to: @user.email, subject: 'メールのタイトルがここに入ります')
+      @mail = "sakaue@matsui-shuzo.co.jp"
+      mail(to: @mail, subject: 'メールのタイトルがここに入ります')
     end
   end
