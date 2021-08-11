46f6768a8015a4e3c40efd35c2290319d9563eaa
shas0910 <hashimoto@matsui-shuzo.co.jp>
2021-08-11 16:53:08 +0900

    メイン調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users,only:[:index,:edit,:update,]
-    get '/users/sign_out' => 'devise/sessions#destroy'
+  resources :users,only:[:index,:edit,:update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create,:edit, :update, :destroy]
     namespace :api do
